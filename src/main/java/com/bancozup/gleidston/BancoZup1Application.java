e0ef9dac8cb85250c90ac7375d284ffdca6f0df3
gleidston <king2010-ro@hotmail.com>
2020-12-31 01:55:50 -0200

    Initial comment

---
+++ src/main/java/com/bancozup/gleidston/BancoZup1Application.java
@@ -0,0 +1,13 @@
+package com.bancozup.gleidston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoZup1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoZup1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancozup/gleidston/BancoZup1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancozup.gleidston;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoZup1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
